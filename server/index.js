d78e1515c81b1d223ca7d1e3c3688875679b062f
djjd13 <jdeggert98@gmail.com>
2023-03-01 21:34:50 -0600

    Removed MongoDB Conn String

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL = "mongodb+srv://jdeggert:jdeggert456@mern-socialapp.mlyhtgw.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL)
